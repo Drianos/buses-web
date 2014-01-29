9b1e98169a1b002fa6d75a26aa9e1186c9c7872a
drianos <drianhacon173@gmail.com>
2014-01-29 13:18:49 -0600

    push dinamically

---
+++ js/page.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+                 $("#paradas").click(function(){
+                     $("#page-content-wrapper").load("leaflet-map.html");
+                 });
+            });
