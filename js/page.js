990e38f2e7595b5c23f258d8ebbe761da7e3b427
drianos <drianhacon173@gmail.com>
2014-01-29 13:39:36 -0600

    take out content of main page

--- js/page.js
+++ js/page.js
@@ -1,3 +1,5 @@
+$("#page-content-wrapper").load("content.html");
+
 $(document).ready(function(){
                  $("#paradas").click(function(){
                      $("#page-content-wrapper").load("leaflet-map.html");
