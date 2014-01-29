b1de15ab7460511fa34f440a4b3820d38bbc8e5a
drianos <drianhacon173@gmail.com>
2014-01-29 13:49:09 -0600

    added extra files for content

--- js/page.js
+++ js/page.js
@@ -1,7 +1,27 @@
 $("#page-content-wrapper").load("content.html");
 
+
+$(document).ready(function(){
+                 $("#contenido").click(function(){
+                     $("#page-content-wrapper").load("content.html");
+                 });
+            });
+$("#page-content-wrapper").load("contenido.html");
+
+$(document).ready(function(){
+                 $("#horarios").click(function(){
+                     $("#page-content-wrapper").load("horarios.html");
+                 });
+            });
+
 $(document).ready(function(){
                  $("#paradas").click(function(){
                      $("#page-content-wrapper").load("leaflet-map.html");
+                 });
+            });
+
+$(document).ready(function(){
+                 $("#acerca").click(function(){
+                     $("#page-content-wrapper").load("acerca.html");
                  });
             });
