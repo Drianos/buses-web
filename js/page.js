55d3588967b5e63710869a2b48576c7ebb4cb6ad
drianos <drianhacon173@gmail.com>
2014-01-29 17:14:42 -0600

    revert changes

--- js/page.js
+++ js/page.js
@@ -5,8 +5,6 @@ $(document).ready(function(){
                  $("#contenido").click(function(){
                      $("#page-content-wrapper").load("content.html");
                  });
-                 e.preventDefault();
-        		$("#wrapper").toggleClass("active");
             });
 $("#page-content-wrapper").load("contenido.html");
 
@@ -14,22 +12,16 @@ $(document).ready(function(){
                  $("#horarios").click(function(){
                      $("#page-content-wrapper").load("horarios.html");
                  });
-                 e.preventDefault();
-        		$("#wrapper").toggleClass("active");
             });
 
 $(document).ready(function(){
                  $("#paradas").click(function(){
                      $("#page-content-wrapper").load("leaflet-map.html");
                  });
-                 e.preventDefault();
-        		$("#wrapper").toggleClass("active");
             });
 
 $(document).ready(function(){
                  $("#acerca").click(function(){
                      $("#page-content-wrapper").load("acerca.html");
                  });
-                 e.preventDefault();
-        		$("#wrapper").toggleClass("active");
             });
